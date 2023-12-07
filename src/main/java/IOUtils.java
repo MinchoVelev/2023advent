e7699de1ec08af6c935989be297daac5dd27fbb7
Mincho Velev <mincho.velev@sap.com>
2023-12-07 20:14:58 +0200

    Init

---
+++ src/main/java/IOUtils.java
@@ -0,0 +1,9 @@
+import java.io.BufferedInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class IOUtils {
+    public static Scanner getScanner(String fileName) {
+        return new Scanner(new BufferedInputStream(IOUtils.class.getResourceAsStream(fileName)), StandardCharsets.UTF_8);
+    }
+}
