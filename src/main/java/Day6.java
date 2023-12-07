2780c071c8e11099ee4705b1d902e61c5c2967a9
Mincho Velev <mincho.velev@sap.com>
2023-12-07 20:36:39 +0200

    Day6

--- src/main/java/Day6.java
+++ src/main/java/Day6.java
@@ -1,138 +1,57 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Day6 {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = IOUtils.getScanner("day5.txt");
-        long[] seeds = Arrays.stream(scanner.nextLine().split(":")[1].trim().split("\\s+")).mapToLong(Long::valueOf).toArray();
-
-        ArrayList<Mapper> mappers = createMappers(scanner);
-        long min = Long.MAX_VALUE;
-
-        for(int i = 0; i < seeds.length; i++){
-                long mappedValue = seeds[i];
-                for (Mapper m : mappers) {
-                    mappedValue = m.map(mappedValue);
-                }
-                if(mappedValue < min){
-                    min = mappedValue;
-                }
-        }
-
-        System.out.println("Part 1: " + min);
-
-
-        long min2 = Long.MAX_VALUE;
-
-        for(int i = 0; i < seeds.length; i+= 2){
-            for (long j = seeds[i]; j <  seeds[i] + seeds[i+1]; j++){
-
-
-            long mappedValue = j;
-            for (Mapper m : mappers) {
-                mappedValue = m.map(mappedValue);
-            }
-            if(mappedValue < min2){
-                min2 = mappedValue;
-            }
-            }
-        }
-
-        System.out.println("Part 2: " + min2);
+        part1();
+        part2();
     }
 
-    private static ArrayList<Mapper> createMappers(Scanner scanner) {
-        ArrayList<Mapper> mappers = new ArrayList<>(7);
-        for(int i = 0; i < 7; i++){
-            mappers.add(i, new Mapper());
+    private static void part2() {
+        Scanner scanner = IOUtils.getScanner("day6.txt");
+
+        String timesPlain = scanner.nextLine();
+        String distancesPlain = scanner.nextLine();
+
+        scanner.close();
+
+        long time = Long.valueOf(timesPlain.split(":")[1].replaceAll("\\s+", ""));
+        long distance = Long.valueOf(distancesPlain.split(":")[1].replaceAll("\\s+", ""));
+
+
+        long result = 0;
+        for (int j = 1; j < time; j++) {
+            if( ((time - j) * j) > distance){
+                result++;
+            }
         }
 
-        int index = -1;
-        while(scanner.hasNext()) {
-            String line = scanner.nextLine();
-            if(line.isBlank()){
-                continue;
-            }
-            if(line.contains("map:")){
-                index++;
-                continue;
-            }
-
-            long[] mappingValues = Arrays.stream(line.split("\\s+")).mapToLong(Long::valueOf).toArray();
-            mappers.get(index).putInterval(mappingValues[1], mappingValues[0], mappingValues[2]);
-        }
-        return mappers;
+        System.out.println("Part2: " + result);
     }
 
-    static class Mapper{
-        Node root;
-        void putInterval(long sourceIntervalStart, long targetIntervalStart, long intervalSize){
-           root = insert(root, sourceIntervalStart, sourceIntervalStart + intervalSize - 1, targetIntervalStart - sourceIntervalStart);
+    private static void part1() {
+        Scanner scanner = IOUtils.getScanner("day6.txt");
+
+        String timesPlain = scanner.nextLine();
+        String distancesPlain = scanner.nextLine();
+
+        scanner.close();
+
+        int[] times = Arrays.stream(timesPlain.split(":")[1].trim().split("\\s+")).mapToInt(Integer::valueOf).toArray();
+        int[] distances = Arrays.stream(distancesPlain.split(":")[1].trim().split("\\s+")).mapToInt(Integer::valueOf).toArray();
+
+        int result = 1;
+        for (int i = 0; i < times.length; i++) {
+            LinkedList<Integer> raceResult = new LinkedList<>();
+            for (int j = 1; j < times[i]; j++) {
+                raceResult.add((times[i] - j) * j);
+            }
+            int distanceToBeat = distances[i];
+            result *= raceResult.stream().filter(s -> s > distanceToBeat).count();
         }
 
-        Node insert(Node parent, long sourceIntervalStart, long sourceIntervalEnd, long diff ){
-            if (parent == null){
-                return new Node(sourceIntervalStart, sourceIntervalEnd, diff);
-            }
-            if(parent.intervalStart > sourceIntervalStart){
-
-             parent.left = insert(parent.left, sourceIntervalStart, sourceIntervalEnd, diff);
-
-            }else if (parent.intervalStart < sourceIntervalStart){
-
-                    parent.right = insert(parent.right, sourceIntervalStart, sourceIntervalEnd, diff);
-            }else {
-                return parent;
-            }
-            return parent;
-        }
-
-        void print(Node n){
-            if(n == null){
-                return;
-            }
-            System.out.printf("node [%d,%d] ", n.intervalStart, n.intervalEnd);
-            if(n.left != null)
-            System.out.printf("left [%d,%d] ", n.left.intervalStart, n.left.intervalEnd);
-            if(n.right != null)
-            System.out.printf("right [%d,%d] ", n.right.intervalStart, n.right.intervalEnd);
-            System.out.println();
-            System.out.println();
-            print(n.left);
-            print(n.right);
-        }
-
-        long map(long number){
-           long mapping = findMapping(root, number);
-           return mapping + number;
-        }
-
-        private long findMapping(Node node, long number) {
-            if(node == null){
-                return 0;
-            }
-            if(node.intervalStart <= number && node.intervalEnd >= number){
-                return node.diff;
-            }
-
-            if(node.intervalStart >= number){
-                return findMapping(node.left, number);
-            }
-            return findMapping(node.right, number);
-        }
-    }
-    static class Node{
-        public Node(long intervalStart, long intervalEnd, long diff) {
-            this.intervalStart = intervalStart;
-            this.intervalEnd = intervalEnd;
-            this.diff = diff;
-        }
-
-        long intervalStart;
-        long intervalEnd;
-        long diff;
-        Node left;
-        Node right;
+        System.out.println("Part1: " + result);
     }
 }
